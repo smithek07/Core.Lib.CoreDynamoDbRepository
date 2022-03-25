ad8c0ed76f631941cda3a1b2808fd52c4259c077
eksrikanthproject <eksrikanth.project@gmail.com>
2022-03-24 20:51:16 -0700

    initial commit

---
+++ Core.Lib.CoreDynamoDbRepository/Core/DynamoDbRepository.cs
@@ -0,0 +1,58 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Core.Lib.CoreDynamoDbRepository.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Lib.CoreDynamoDbRepository.Core
+{
+    public class DynamoDbRepository<T> : IDynamoDbRepository<T> where T : new()
+    {
+        private readonly IDynamoDBContext dynamoDBContext;
+
+        /// <summary>
+        /// Create new instance of the <see cref=" DynamoDbRepository{T}"/>
+        /// </summary>
+        /// <param name="dynamoDbTableName"></param>
+        public DynamoDbRepository(string dynamoDbTableName) : this(dynamoDbTableName, new AmazonDynamoDBClient())
+        {
+        }
+
+        /// <summary>
+        /// Create new instance of the <see cref=" DynamoDbRepository{T}"/>
+        /// </summary>
+        /// <param name="dynamoDbTableName"></param>
+        /// <param name="dynamoDBClient"></param>
+        public DynamoDbRepository(string dynamoDbTableName, IAmazonDynamoDB dynamoDBClient)
+        {
+            if(!string.IsNullOrEmpty(dynamoDbTableName))
+            {
+                AWSConfigsDynamoDB.Context.TypeMappings[typeof(T)] =
+                    new Amazon.Util.TypeMapping(typeof(T), dynamoDbTableName);
+            }
+            var config = new DynamoDBContextConfig { Conversion = DynamoDBEntryConversion.V2 };
+            dynamoDBContext = new DynamoDBContext(dynamoDBClient, config);
+        }
+
+        public async Task CreateOrUpdateAsync(T obj)
+        {
+            await dynamoDBContext.SaveAsync(obj);
+        }
+
+        public async Task DeleteAsync(T obj)
+        {
+            await dynamoDBContext.DeleteAsync(obj);
+        }
+
+        public async Task<IList<T>> GetAllAsync()
+        {
+            return await dynamoDBContext.ScanAsync<T>(null).GetRemainingAsync();
+        }
+
+        public async Task<T> GetAsync(string key)
+        {
+            return await dynamoDBContext.LoadAsync<T>(key);
+        }
+    }
+}
---
+++ Core.Lib.CoreDynamoDbRepository/Interface/IDynamoDbRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Lib.CoreDynamoDbRepository.Interface
+{
+    public interface IDynamoDbRepository<T> where T : new ()
+    {
+        /// <summary>
+        /// Fetch data from DynamoDB.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>Data for <typeparamref name="T"/> from DynamoDB</returns>
+        Task<T> GetAsync(string key);
+
+        /// <summary>
+        /// Fetch collection of data for from DynamoDB
+        /// </summary>
+        /// <returns>Collection of data for <typeparamref name="T"/> from DynamoDB</returns>
+        Task<IList<T>> GetAllAsync();
+
+        /// <summary>
+        /// Insert or Update DynamoDB data.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Void Task</returns>
+        Task CreateOrUpdateAsync(T obj);
+
+        /// <summary>
+        /// Delete a record from DynamoDB.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Void Task</returns>
+        Task DeleteAsync(T obj);
+
+    }
+}
